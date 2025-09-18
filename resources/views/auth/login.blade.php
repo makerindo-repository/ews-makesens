6f2153a3dbfd64c91c131e7da9baa7712ee0c11f
fahrifuh <muhammadfahri571@gmail.com>
2025-09-18 13:51:19 +0700

    feat(auth): menyesuaikan halaman login sesuai custom design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,37 +5,83 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
+        <div class="mb-4">
+            <p class="text-3xl font-bold text-[#0751A6]">Login</p>
+            <p class="text-sm text-slate-500">Silakan masuk dengan akun Anda.</p>
+        </div>
+
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <div class="relative mt-1">
+                <!-- Icon -->
+                <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-400">
+                    <i class="fa-solid fa-envelope"></i>
+                </span>
+
+                <x-text-input id="email" class="block w-full pl-12" type="email" name="email" :value="old('email')"
+                    required autofocus autocomplete="username" placeholder="email@example.com" />
+            </div>
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
+            {{-- 
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" /> --}}
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <div class="relative mt-1">
+                <!-- Icon -->
+                <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-400">
+                    <i class="fa-solid fa-key"></i>
+                </span>
 
+                <!-- Input -->
+                <x-text-input id="password" class="block w-full pl-12 pr-10" type="password" name="password" required
+                    autocomplete="current-password" placeholder="minimal 8 karakter" />
+
+                <!-- Toggle visibility -->
+                <span class="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-400 cursor-pointer" id="togglePassword">
+                    <i class="fa-solid fa-eye"></i>
+                </span>
+            </div>
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <input id="remember_me" type="checkbox"
+                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
         </div>
 
         <div class="flex items-center mt-4">
-            <x-primary-button class="">
-                {{ __('Log in') }}
+            <x-primary-button>
+                {{ __('Login') }}
+                <i class="fa-solid fa-arrow-right"></i>
             </x-primary-button>
         </div>
     </form>
+    @push('scripts')
+        <script>
+            document.addEventListener("DOMContentLoaded", () => {
+                const toggle = document.querySelector("#togglePassword");
+                const passwordInput = document.querySelector("#password");
+                const icon = toggle.querySelector("i");
+
+                toggle.addEventListener("click", () => {
+                    const isPassword = passwordInput.type === "password";
+                    passwordInput.type = isPassword ? "text" : "password";
+
+                    // toggle icon
+                    icon.classList.toggle("fa-eye");
+                    icon.classList.toggle("fa-eye-slash");
+                });
+            });
+        </script>
+    @endpush
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,30 +9,49 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet">
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
-    <div class="relative min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+    <div class="relative min-h-screen flex flex-col justify-center items-center md:items-start pt-6 sm:pt-0">
         <!-- Layer 1: Background image -->
         <div class="absolute inset-0">
             <img src="{{ asset('images/bgLogin.jpg') }}" alt="Background" class="w-full h-full object-cover">
         </div>
 
         <!-- Layer 2: Gradient overlay -->
-        <div class="absolute inset-0 bg-gradient-to-r from-blue-900/80 via-blue-900/40 to-transparent"></div>
+        <div class="absolute inset-0 bg-gradient-to-r from-blue-900/100 via-blue-900/60 to-transparent"></div>
 
         <!-- Layer 3: Content -->
-        <div class="relative z-10 flex flex-col items-center w-full sm:max-w-md px-6">
-            <div class="mt-6 w-full px-6 py-6 bg-white shadow-xl rounded-lg backdrop-blur">
+        <div class="relative z-10 flex flex-col justify-center w-full gap-5 sm:max-w-xl px-6 md:ms-20">
+            <div>
+                <a href="/">
+                    <img src="{{ asset('images/logoMakesens.png') }}" alt="Logo"
+                        class="h-auto w-52 object-contain filter brightness-0 invert">
+                </a>
+            </div>
+            <div>
+                <h1 class="text-white text-5xl font-bold">Welcome to EWS</h1>
+                <p class="text-white font-extralight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, dolor.</p>
+            </div>
+            <div class="w-full px-6 py-8 md:px-12 md:py-10 bg-white shadow-xl rounded-lg backdrop-blur">
                 {{ $slot }}
             </div>
         </div>
     </div>
+    @stack('scripts')
 </body>
 
 </html>
