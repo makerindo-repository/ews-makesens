bad5b0c7a3db33dea5806f1ed629825d053b91d2
fahrifuh <muhammadfahri571@gmail.com>
2025-09-23 09:17:49 +0700

    feat(iot-node): mendefinisikan route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\IoTNodeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('iot-node', IoTNodeController::class)->except('show')->names([
+        'index' => 'iot-node.index',
+        'create' => 'iot-node.create',
+        'store' => 'iot-node.store',
+        'edit' => 'iot-node.edit',
+        'update' => 'iot-node.update',
+        'destroy' => 'iot-node.destroy',
+    ]);
 });
 
 require __DIR__ . '/auth.php';
