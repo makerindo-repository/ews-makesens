ab41b1b6c030fab4b1286b378aa9874358e1932e
fahrifuh <muhammadfahri571@gmail.com>
2025-09-24 13:51:07 +0700

    feat(iot_node): membuat migration

---
+++ database/migrations/2025_09_23_033635_create_iot_nodes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('iot_nodes', function (Blueprint $table) {
+            $table->id();
+            $table->string('serial_number')->unique();
+            $table->string('name');
+            $table->foreignId('location_id')->constrained('locations')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('io_t_nodes');
+    }
+};
