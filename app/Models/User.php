026f54f86a71b06cb1125f8abf415daaa58c914f
fahrifuh <muhammadfahri571@gmail.com>
2025-09-25 11:06:57 +0700

    feat(user): menambah field untuk fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,11 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'phone',
         'password',
+        'profile_picture',
+        'address',
+        'role',
     ];
 
     /**
