480ac82188ee49c5536996d2861cfc745b912725
fahrifuh <muhammadfahri571@gmail.com>
2025-09-28 20:08:56 +0700

    feat(pub_node): membuat model

---
+++ app/Models/PublicNode.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PublicNode extends Model
+{
+    use HasFactory;
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function node()
+    {
+        return $this->belongsTo(IoTNode::class, 'iot_node_id');
+    }
+}
