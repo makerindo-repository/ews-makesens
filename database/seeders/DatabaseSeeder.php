f7970897e8337b70d929bf2c883fdd4299e48abb
fahrifuh <muhammadfahri571@gmail.com>
2025-09-30 21:26:26 +0700

    feat(seeder): membuat seeder untuk user admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,14 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'profile_picture' => 'images/profile-picture/embung.jpg',
+            'phone' => '081320919314', 
+            'role' => 'superadmin',
+            'address' => 'Margahayu, Bandung',
+            'gender' => 'l',
         ]);
     }
 }
