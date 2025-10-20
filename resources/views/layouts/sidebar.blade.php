abfcf443ecb5b131f4cfbbed8d4d5845008acaeb
fahrifuh <muhammadfahri571@gmail.com>
2025-10-20 10:14:54 +0700

    feat(sidebar): show menu untuk laporan, styling menu active

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -25,7 +25,15 @@
                     Data & Monitoring
                 </li>
 
-                <li x-data="{ open: false }" class="menu-item w-full">
+                <li x-data="{
+                    open: {{ request()->routeIs('iot-node.*') ||
+                    request()->routeIs('public-node.*') ||
+                    request()->routeIs('location.*') ||
+                    request()->routeIs('volunteer.*') ||
+                    request()->routeIs('raw-data.*')
+                        ? 'true'
+                        : 'false' }}
+                }" class="menu-item w-full">
                     <button @click="open = !open" :aria-expanded="open"
                         class="menu-link w-full flex items-center pe-10 py-2 text-left">
                         <i class="menu-icon fa-solid fa-database"></i>
@@ -43,39 +51,54 @@
                         x-transition:leave-start="opacity-100 translate-y-0"
                         x-transition:leave-end="opacity-0 -translate-y-1">
                         <li>
-                            <a href="{{ route('iot-node.index') }}" class="menu-link">
+                            <a href="{{ route('iot-node.index') }}" @class([
+                                'menu-link',
+                                'active-icon' => request()->routeIs('iot-node.*'),
+                            ])>
                                 <div class="text-sm"><span class="me-2">--</span> Data IoT Node</div>
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('public-node.index') }}" class="menu-link">
+                            <a href="{{ route('public-node.index') }}" @class([
+                                'menu-link',
+                                'active-icon' => request()->routeIs('public-node.*'),
+                            ])>
                                 <div class="text-sm"><span class="me-2">--</span> Data Publik Node</div>
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('location.index') }}" class="menu-link">
+                            <a href="{{ route('location.index') }}" @class([
+                                'menu-link',
+                                'active-icon' => request()->routeIs('location.*'),
+                            ])>
                                 <div class="text-sm"><span class="me-2">--</span> Data Lokasi</div>
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('volunteer.index') }}" class="menu-link">
+                            <a href="{{ route('volunteer.index') }}" @class([
+                                'menu-link',
+                                'active-icon' => request()->routeIs('volunteer.*'),
+                            ])>
                                 <div class="text-sm"><span class="me-2">--</span> Data Relawan</div>
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('raw-data.index') }}" class="menu-link">
+                            <a href="{{ route('raw-data.index') }}" @class([
+                                'menu-link',
+                                'active-icon' => request()->routeIs('raw-data.*'),
+                            ])>
                                 <div class="text-sm"><span class="me-2">--</span> Data Raw</div>
                             </a>
                         </li>
                     </ul>
                 </li>
 
-                {{-- <li class="menu-item">
-                    <a href="#" class="menu-link">
+                <li class="menu-item">
+                    <a href="{{ route('report.index') }}" @class(['menu-link', 'active-icon' => request()->routeIs('report.*')])>
                         <i class="menu-icon fa-solid fa-clipboard"></i>
                         <div class="text-base">Laporan</div>
                     </a>
-                </li> --}}
+                </li>
 
                 <!-- Group: Konfigurasi -->
                 <li class="mt-6 px-6 text-xs font-semibold text-slate-500 uppercase tracking-wider">
@@ -83,20 +106,25 @@
                 </li>
 
                 <li class="menu-item">
-                    <a href="{{ route('user.index') }}" class="menu-link">
+                    <a href="{{ route('user.index') }}" class="menu-link" @class(['menu-link', 'active-icon' => request()->routeIs('user.*')])>
                         <i class="menu-icon fa-solid fa-users"></i>
                         <div class="text-base">Manajemen User</div>
                     </a>
                 </li>
 
                 <li class="menu-item">
-                    <a href="{{ route('weather.index') }}" class="menu-link">
+                    <a href="{{ route('weather.index') }}" class="menu-link" @class([
+                        'menu-link',
+                        'active-icon' => request()->routeIs('weather.*'),
+                    ])>
                         <i class="menu-icon fa-solid fa-cloud"></i>
                         <div class="text-base">Manajemen Data Cuaca</div>
                     </a>
                 </li>
 
-                <li x-data="{ open: false }" class="menu-item w-full">
+                <li x-data="{
+                    open: {{ request()->routeIs('app-setting.*') || request()->routeIs('threshold.*') ? 'true' : 'false' }}
+                }" class="menu-item w-full">
                     <button @click="open = !open" :aria-expanded="open"
                         class="menu-link w-full flex items-center pe-10 py-2 text-left">
                         <i class="menu-icon fa-solid fa-gear"></i>
@@ -115,12 +143,18 @@
                         x-transition:leave-end="opacity-0 -translate-y-1">
 
                         <li>
-                            <a href="{{ route('app-setting.index') }}" class="menu-link">
+                            <a href="{{ route('app-setting.index') }}" @class([
+                                'menu-link',
+                                'active-icon' => request()->routeIs('app-setting.*'),
+                            ])>
                                 <div class="text-sm"><span class="me-2">--</span> Pengaturan Aplikasi</div>
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('threshold.index') }}" class="menu-link">
+                            <a href="{{ route('threshold.index') }}" @class([
+                                'menu-link',
+                                'active-icon' => request()->routeIs('threshold.*'),
+                            ])>
                                 <div class="text-sm"><span class="me-2">--</span> Pengaturan Threshold</div>
                             </a>
                         </li>
@@ -129,7 +163,10 @@
 
 
                 <li class="menu-item">
-                    <a href="{{ route('activity-log.index') }}" class="menu-link">
+                    <a href="{{ route('activity-log.index') }}" class="menu-link" @class([
+                        'menu-link',
+                        'active-icon' => request()->routeIs('activity-log.*'),
+                    ])>
                         <i class="menu-icon fa-solid fa-clock-rotate-left"></i>
                         <div class="text-base">Log Aktivitas</div>
                     </a>
